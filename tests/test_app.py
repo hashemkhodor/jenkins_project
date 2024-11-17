5c84e83cfd5e41b870fb05c84cb7c9e8cdaf7ebc
Hashem Khodor <hashemkhoder1@gmail.com>
2024-11-17 11:23:50 +0200

    Changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,15 @@
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from app import greet
 
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World"), "Hello, World!")
 
 if __name__== "__main__":
     unittest.main()
